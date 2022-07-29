a7c8d872afb7ccf38dbcdf76c2dcd612b64e8755
Tuan Au <audtuan@gmail.com>
2022-07-29 13:18:14 +0700

    first commit

---
+++ DownloadSketchCloudFiles.js
@@ -0,0 +1,28 @@
+// scroll to Bottom page
+const SCROLLTIME = 2000;
+const DOWNLOADTIME = 3000;
+const scrollArea = document.querySelector('aside').nextElementSibling;
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+async function scrollToBottom() {
+    if(scrollArea.scrollHeight - scrollArea.scrollTop === scrollArea.clientHeight) {
+        // start Download
+        let fileCards = [...scrollArea.querySelectorAll('div[draggable="true"]')];
+        if(fileCards.length == 0) fileCards = [...scrollArea.querySelectorAll('tr[role="button"]')];
+        console.log(`Download total of ${fileCards.length} files`);
+        for (let i = 0; i < fileCards.length; i++) {
+            let button = fileCards[i].querySelector('button');
+            let title = fileCards[i].querySelector('div[title]').getAttribute('title');
+            if(button) {
+                button.click();
+                console.log(`👍 Download ${i+1}/${fileCards.length}: ${title}`);
+                let downloadLink = document.querySelector('div[data-placement] ul[data-testid] a');
+                if(downloadLink.innerHTML == 'Download') downloadLink.click();
+                await sleep(DOWNLOADTIME);
+            }
+        }
+    } else {
+        scrollArea.scrollTop = scrollArea.scrollHeight;
+        setTimeout(scrollToBottom, SCROLLTIME);
+    }
+}
+setTimeout(scrollToBottom, 0);
